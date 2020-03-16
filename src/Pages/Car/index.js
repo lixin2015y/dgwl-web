2c481a59523b05602adededc6f6205fa762c27d7
lixin2015y <lixin2015y@163.com>
2020-03-16 23:28:39 +0800

    11

--- src/Pages/Car/index.js
+++ src/Pages/Car/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Button, Card, Col, Form, Input, InputNumber, Layout, Row, Select, Table} from "antd";
+import React, {Component} from 'react';
+import {Button, Card, Col, Form, Input, InputNumber, Layout, message, Modal, Row, Select, Table} from "antd";
 import {sendRequest} from "../../NetRequest/api";
 
 const {Option} = Select;
@@ -8,7 +8,9 @@ class Car extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            carLoading: false
+            carLoading: true,
+            modalVisible: false,
+            tableIndex: 0
         }
     }
 
@@ -33,17 +35,25 @@ class Car extends React.Component {
             title: '容积(立方米)',
             dataIndex: 'capacity',
             key: 'capacity'
+        }, {
+            title: '创建时间',
+            dataIndex: 'updateTime',
+            key: 'updateTime'
         }
     ]
 
+    componentDidMount() {
+        this.query()
+    }
+
     render() {
         const {getFieldDecorator} = this.props.form;
-        const {carLoading, data} = this.state;
+        const {carLoading, data, modalVisible, tableIndex} = this.state;
         return (
             <Layout style={{height: '100%', width: '100%', overflow: 'inherit', background: '#fff'}}>
                 <Card style={{borderRadius: '10px', background: '#F7F7F7'}}>
                     <Row>
-                        <Form layout="inline" onSubmit={this.handleSubmit} >
+                        <Form layout="inline" onSubmit={this.handleSubmit}>
                             <Col span={5}>
                                 <Form.Item label={'车牌号：'}>
                                     {getFieldDecorator('number', {initialValue: ''})
@@ -67,22 +77,60 @@ class Car extends React.Component {
                                     (<InputNumber/>)}
                                 </Form.Item>
                             </Col>
-                            <Col span={3}><Form.Item>
-                                <Button type="primary" htmlType="submit">
-                                    查询
-                                </Button>
-                            </Form.Item></Col>
+                            <Col span={3}>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit">
+                                        查询
+                                    </Button>
+                                </Form.Item>
+                            </Col>
+                            <Col span={3}>
+                                <Form.Item>
+                                    <Button type="primary" onClick={this.changeModalVisible}>
+                                        添加
+                                    </Button>
+                                </Form.Item>
+                            </Col>
                         </Form>
                     </Row>
                 </Card>
                 <Row>
                     <div style={{marginTop: '20px', height: 'calc(100% - 280px)'}}>
-                        <Table columns={this.column} bordered loading={carLoading} dataSource={data}/>
+                        <Table columns={this.column} bordered loading={carLoading} dataSource={data}
+                               key={tableIndex} rowKey={record => record.id}/>
                     </div>
                 </Row>
-
+                <Modal title={'添加新车辆'} visible={modalVisible} onCancel={this.changeModalVisible}
+                       onOk={this.handleModalSubmit} okText={'添加'} cancelText={'取消'}
+                >
+                    <ModaForm wrappedComponentRef={(ref) => {
+                        this.formRef = ref
+                    }}/>
+                </Modal>
             </Layout>
         )
+    }
+
+    handleModalSubmit = () => {
+        const {form} = this.formRef.props
+        form.validateFields((err, values) => {
+            if (!err) {
+                sendRequest('/car/addCar', 'post', values).then((data) => {
+                    if (data.code === '0') {
+                        message.info('添加新车辆成功！！！')
+                        this.changeModalVisible()
+                        this.query()
+                    } else {
+                        message.error(data.message)
+                    }
+                })
+            }
+        })
+    }
+
+    changeModalVisible = () => {
+        const modalVisible = !this.state.modalVisible
+        this.setState({modalVisible})
     }
 
     handleSubmit = (e) => {
@@ -91,16 +139,75 @@ class Car extends React.Component {
     }
 
     query = () => {
-        this.setState({
-            carLoading: true
-        })
-        sendRequest('/car/getCars').then(data => {
-            this.setState({
-                data: data.data,
-                carLoading: false
-            })
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.setState({
+                    carLoading: true
+                })
+                sendRequest('/car/getCars', 'post', {
+                    number: values.number,
+                    type: values.type,
+                    load: values.load
+                }).then(data => {
+                    this.setState({
+                        data: data.data,
+                        carLoading: false,
+                        tableIndex: this.state.tableIndex + 1
+                    })
+                })
+            }
         })
     }
 }
 
 export default Car = Form.create()(Car)
+
+const ModaForm = Form.create()(
+    class extends Component {
+        formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 8},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 16},
+            },
+        }
+
+        render() {
+            const {getFieldDecorator} = this.props.form
+            return (
+                <Form {...this.formItemLayout} wrappedComponentRef={(form) => this.modalForm = form}>
+                    <Form.Item label={'车牌号：'}>
+                        {getFieldDecorator('number', {rules: [{required: true, message: '此项不能为空！'}]})
+                        (<Input placeholder="number" style={{width: '30%'}}/>)}
+                    </Form.Item>
+                    <Form.Item label={'类型：'}>
+                        {getFieldDecorator('type', {
+                            initialValue: '小型车',
+                            rules: [{required: true, message: '请填写车类型!'}],
+                        })
+                        (<Select style={{width: '100px'}}>
+                            <Option value="小型车">小型车</Option>
+                            <Option value="中型车">中型车</Option>
+                            <Option value="大型车">大型车</Option>
+                        </Select>)}
+                    </Form.Item>
+                    <Form.Item label={'承载吨数以上：'}>
+                        {getFieldDecorator('load', {
+                            initialValue: 5, rules: [{required: true, message: '此项不能为空！'}]
+                        })
+                        (<InputNumber/>)}
+                    </Form.Item>
+                    <Form.Item label={'容积(立方米)：'}>
+                        {getFieldDecorator('capacity', {
+                            initialValue: 5, rules: [{required: true, message: '此项不能为空!'}]
+                        })
+                        (<InputNumber/>)}
+                    </Form.Item>
+                </Form>
+            )
+        }
+    }
+)
