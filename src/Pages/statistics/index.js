dcafd12d8f740656dce821ad82b85804ea986159
lixin2015y <lixin2015y@163.com>
2020-04-07 22:49:51 +0800

    1

--- src/Pages/statistics/index.js
+++ src/Pages/statistics/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import echarts from 'echarts/lib/echarts';
 import 'echarts/lib/chart/bar';
 import 'echarts/lib/chart/line';
+import 'echarts/lib/chart/pie'
 import {Col, Row} from "antd";
+import {sendRequest} from "../../NetRequest/api";
 
 class Statistics extends React.Component {
     constructor(props) {
@@ -11,22 +13,73 @@ class Statistics extends React.Component {
     }
 
     componentDidMount() {
-        let myChart = echarts.init(document.getElementById('item1'));
-        myChart.setOption({
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        });
+
+        sendRequest('/order/getOrderCount', 'post')
+            .then(r => {
+                let keys = []
+                let values = []
+                let objData = []
+                r.data.forEach(v => {
+                    keys.push(v.month)
+                    values.push(v.count)
+                    objData.push({
+                        value: v.count, name: v.month
+                    })
+                })
+
+                let myChart1 = echarts.init(document.getElementById('item1'));
+                myChart1.setOption({
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: keys
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: values
+                    }]
+                });
+
+                let myChart2 = echarts.init(document.getElementById('item2'));
+                myChart2.setOption({
+                    series: [
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: '55%',
+                            center: ['50%', '60%'],
+                            data: objData,
+                            emphasis: {
+                                itemStyle: {
+                                    shadowBlur: 10,
+                                    shadowOffsetX: 0,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        }
+                    ]
+                })
+
+                let myChart3 = echarts.init(document.getElementById('item3'));
+                myChart3.setOption({
+                    xAxis: {
+                        type: 'category',
+                        data: keys
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [{
+                        data: values,
+                        type: 'line'
+                    }]
+                })
+            })
+
 
     }
 
@@ -36,13 +89,13 @@ class Statistics extends React.Component {
         return (
             <Row>
                 <Col span={8}>
-                    <div id="item1" style={{width: '100%', height: 400}}>aasdasd</div>
+                    <div id="item1" style={{width: '100%', height: 400}}></div>
                 </Col>
                 <Col span={8}>
-                    <div id="item2" style={{width: '100%'}}>aasdasd</div>
+                    <div id="item2" style={{width: '100%', height: 400}}>aasdasd</div>
                 </Col>
                 <Col span={8}>
-                    <div id="item3" style={{width: '100%'}}>aasdasd</div>
+                    <div id="item3" style={{width: '100%', height: 400}}>aasdasd</div>
                 </Col>
             </Row>
         )
